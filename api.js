7697697127c47a1b8b8207a2dfb5830b8a0b322f
Michel Sebaaly <michelsebaaly4@gmail.com>
2025-02-06 19:05:30 +0200

    adding getStudents and createStudents endpoints

---
+++ api.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const XLSX = require("xlsx");
+const fs = require("fs");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/students", (req, res) => {
+  const fileBuffer = fs.readFileSync("data.xlsx");
+  const workbook = XLSX.read(fileBuffer, { type: "buffer" });
+
+  // Get the first sheet name
+  const sheetName = workbook.SheetNames[0];
+
+  // Get the sheet data
+  const sheet = workbook.Sheets[sheetName];
+
+  // Convert the sheet to JSON
+  const data = XLSX.utils.sheet_to_json(sheet);
+
+  console.log(data);
+  res.send(data);
+});
+
+app.post("/createStudent", function (req, res) {
+  const filePath = "data.xlsx";
+  const workbook = XLSX.readFile(filePath);
+
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+
+  let data = XLSX.utils.sheet_to_json(sheet);
+  console.log(req.body);
+  data.push(req.body);
+
+  const updatedSheet = XLSX.utils.json_to_sheet(data);
+
+  workbook.Sheets[sheetName] = updatedSheet;
+
+  XLSX.writeFile(workbook, filePath);
+
+  console.log("Record added successfully!");
+
+  res.send("OK");
+});
+
+app.listen(3000);
